468589b81b05deda05ee9185311e2138e6f6b2bb
Christiaan Hees <mail@chees.info>
2012-06-21 16:45:05 +0200

    Gravity

--- index.js
+++ index.js
@@ -16,18 +16,17 @@ function animate(lastTime, three) {
   var date = new Date();
   var time = date.getTime();
   var timeDiff = time - lastTime;
-  if (lastTime != 0) {
-    for (var i = 0; i < three.cubes.length; i++) {
-      var cube = three.cubes[i];
-      cube.position.x -= Game.speed * timeDiff;
-      if (cube.position.x < -3000) {
-        cube.position.x = Math.random() * 5000 + 2000;
-        cube.position.z = Math.random() * 800 + 400;
-      }
-      //console.log(cube.position.x);
-    }
-  }
-
+  
+	for (var i = 0; i < three.cubes.length; i++) {
+	  var cube = three.cubes[i];
+	  cube.position.x -= Game.speed * timeDiff;
+	  if (cube.position.x < -3000) {
+			cube.position.x = Math.random() * 5000 + 2000;
+			cube.position.z = Math.random() * 800 + 400;
+	  }
+	  //console.log(cube.position.x);
+	}
+	
   if (Game.keysPressed["left"]) {
     three.sphere.position.x -= Game.ballSpeed * timeDiff;
   }
@@ -35,7 +34,18 @@ function animate(lastTime, three) {
     three.sphere.position.x += Game.ballSpeed * timeDiff;
   }
 
-
+	if (Game.keysPressed["space"]) {
+		three.sphere.position.z = 400;
+		three.sphere.fallSpeed = 0;
+	}
+	
+	three.sphere.fallSpeed += Game.gravity * timeDiff;
+  three.sphere.position.z += three.sphere.fallSpeed * timeDiff;
+	if (collides(three.cubes, three.sphere)) {
+		three.sphere.fallSpeed *= -1 * 1.1;
+		three.sphere.fallSpeed = Math.min(three.sphere.fallSpeed, Game.maxFallSpeed);
+		three.sphere.fallSpeed = Math.max(three.sphere.fallSpeed, -Game.maxFallSpeed);
+	}
 
   lastTime = time;
 
@@ -47,6 +57,20 @@ function animate(lastTime, three) {
     animate(lastTime, three);
   });
 };
+
+function collides(list, object) {
+	for (var i = 0; i < list.length; i++) {
+	  var item = list[i];
+		if (item.position.x - item.width / 2 < object.position.x &&
+				item.position.x + item.width / 2 > object.position.x &&
+				item.position.z + item.height / 2 > object.position.z - object.boundRadius &&
+				item.position.z - item.height / 2 < object.position.z + object.boundRadius) {
+			object.position.z = item.position.z + item.height / 2 + object.boundRadius;
+			return true;
+		}
+	}
+	return false;
+}
 
 window.onload = function () {
 
@@ -74,6 +98,9 @@ window.onload = function () {
     var cube = new THREE.Mesh(new THREE.CubeGeometry(400, 50, 20), blueMaterial);
     cube.position.x = Math.random() * 5000 + 2000;
     cube.position.z = Math.random() * 800 - 400;
+		cube.width = 400;
+		cube.height = 20;
+		cube.depth = 50;
     scene.add(cube);
     cubes.push(cube);
   }
@@ -82,7 +109,8 @@ window.onload = function () {
   // sphere
 
   var sphere = new THREE.Mesh(new THREE.SphereGeometry(10, 32, 32), greenMaterial);
-  sphere.position.z = 100;
+  sphere.position.z = 1000;
+	sphere.fallSpeed = 0;
   scene.add(sphere);
 
   // create a point light
@@ -107,17 +135,19 @@ window.onload = function () {
 
   initKeyboardEvent();
 
-  animate(0, Game.three);
+  animate(+new Date(), Game.three);
 };
 
 var initKeyboardEvent = function () {
   window.addEventListener("keydown", function (event) {
-    if (event.keyCode == 37) { Game.keysPressed["left"] = true; return; }
-    if (event.keyCode == 39) { Game.keysPressed["right"] = true; return; }
+    if (event.keyCode === 37) { Game.keysPressed["left"] = true; return; }
+    if (event.keyCode === 39) { Game.keysPressed["right"] = true; return; }
+		if (event.keyCode === 32) { Game.keysPressed["space"] = true; return; }
   }, false);
   window.addEventListener("keyup", function (event) {
     if (event.keyCode === 37) { Game.keysPressed["left"] = false; return; }
     if (event.keyCode === 39) { Game.keysPressed["right"] = false; return; }
+		if (event.keyCode === 32) { Game.keysPressed["space"] = false; return; }
   }, false);
 };
 
@@ -125,9 +155,8 @@ var initKeyboardEvent = function () {
 var Game = {
   three: undefined,
   speed: 0.4,
-  gravity: -0.4,
+  gravity: -0.0008,
   ballSpeed: 0.7,
-  ballMaxGravity: 1,
+  maxFallSpeed: 1,
   keysPressed: new Array(),
-
 };
